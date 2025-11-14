1c485b8b84753661317f37f64dfb7310dc3e46a5
tenze21 <tenzinchoda365@gmail.com>
2025-11-14 19:22:18 +0600

    ESM module directory import fix

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,11 +1,11 @@
 // Types
-export * from './types';
+export * from './types/index.js';
 
 // Schema and validation
-export * from './schemas';
+export * from './schemas/index.js';
 
 // Constants
-export * from './constants';
+export * from './constants/index.js';
 
 // Utils (we'll add these later)
 // export * from './utils';
